91e1e46fdb2689b130982631f8765b6dba022b3b
Michael Ferolito <ferolito@wisc.edu>
2023-09-24 12:00:10 -0500

    Import previous test code

--- testRunner.cpp
+++ testRunner.cpp
@@ -1,8 +1,17 @@
 #include "mbed.h"
 
+// int get_temp(int pin, int R){
+//     int m;
+//     int b;
+//     // TODO read pin
+//     // 5 is vcc
+
+//     int vcc = 5;
+    
+// }
+
+
 int main()
 {
-    printf("Hello World!\n");
-
-    while(1) {}
+    puts("INITIALIZED");
 }
