af3a5d0ac03dcfd88b17f155bd1824b96b5d74d9
Michael Ferolito <ferolito@wisc.edu>
2023-09-29 19:00:12 -0500

    Set up server connections

--- drivers/include/ethernet.h
+++ drivers/include/ethernet.h
@@ -5,27 +5,54 @@
 Server implementation on Nucleo for communication with the pi
 */
 
+#include "SocketAddress.h"
 #include "mbed.h"
+#include "NetworkInterface.h"
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdint.h"
 #include "limits.h"
 #include "stdbool.h"
 
+#define ENET_ERR_OK         ((int8_t)0)
+#define ENET_ERR_NO_CONNECT ((int8_t)1)
+#define ENET_ERR_SOCK_BROKE ((int8_t)2)
+#define ENET_ERR_INVALID    ((int8_t)3)
+
+#define NETMASK     "255.255.255.0"
+#define GATEWAY     "0.0.0.0"
+
+// struct enet_err_t{
+//     int32_t err_code;
+//     char *msg;
+// };
+
 class EthernetServer{
     public:
-        EthernetServer(const char *this_ip, const char* client_ip, void (*data_recv)(void *data, int size), void (*data_write)(int *size));
-        void init(const char *this_ip, const char* client_ip, void (*data_recv)(void *data, int size), void (*data_write)(int *size));
-        void run(void);
+        EthernetServer(const char *this_ip, int port, void (*data_recv)(void *data, int size), void (*data_write)(int *size));
+        void init(const char *this_ip, int port, void (*data_recv)(void *data, int size), void (*data_write)(int *size));
+        int8_t run(void);
         char ip[48];
-        char client_ip[48];
         void (*data_recv)(void *data, int size);
         void (*data_write)(int *size);
     private:
         void read_thread(void);
         void write_thread(void);
+        int port;
         char sbuffer[512];
         char rbuffer[512];
+
+        NetworkInterface *net;
+
+        SocketAddress *server_address;
+        
+        TCPSocket server;
+        TCPSocket *client_socket;
+        SocketAddress client_address;
+
+        
+
 };
 
 #endif /*ETHERNET_H*/
--- drivers/src/ethernet.cpp
+++ drivers/src/ethernet.cpp
@@ -1,4 +1,9 @@
 #include "ethernet.h"
+#include "EthernetInterface.h"
+#include "NetworkInterface.h"
+#include "Socket.h"
+#include "TCPSocket.h"
+#include "nsapi_types.h"
 
 /**
  * Initializes an EthernetServer
@@ -7,19 +12,41 @@
  * data_recv -- function to call when data is received
  * data_write -- function to call in order to get data to send
  */
-EthernetServer::EthernetServer(const char *this_ip, const char* client_ip, 
+EthernetServer::EthernetServer(const char *this_ip, int port,
     void (*data_recv)(void *data, int size), void (*data_write)(int *size)){
-    init(this_ip, client_ip, data_recv, data_write);
+    init(this_ip, port, data_recv, data_write);
 }
 
-void EthernetServer::init(const char *this_ip, const char *client_ip,
+void EthernetServer::init(const char *this_ip, int port,
  void (*data_recv)(void *, int), void (*data_write)(int *)){
     // Copy ip
     memmove(this->ip, this_ip, strlen(this_ip)+1);
-    // Copy client ip
-    memmove(this->client_ip, client_ip, strlen(client_ip)+1);
     // Callback for when data is received
     this->data_recv = data_recv;
     // Callback for when data needs to be sent
     this->data_write = data_write;
+    // The TCP port to use for communication
+    this->port = port;
+}
+
+int8_t EthernetServer::run(void){
+    puts("Starting ethernet communication driver");
+    puts("Initializing Network Interface");
+    this->net = EthernetInterface::get_default_instance();
+    puts("Generating server address");
+    this->server_address = new SocketAddress(this->ip, this->port);
+    puts("Opening socket on Ethernet Interface...");
+    this->server.open(net);
+    puts("Binding socket to generated address...");
+    this->server.bind(*(this->server_address));
+    puts("Configure as non blocking...");
+    this->server.set_blocking(false);
+    puts("Listening on socket...");
+    this->server.listen();
+    
+    while(1){
+        
+    }
+
+    return 0;
 }
