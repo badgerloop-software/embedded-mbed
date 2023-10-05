ebbe0db67d7897bf1477fec8d8ef19dce85afa10
James <jamesvollmer02@gmail.com>
2023-10-05 12:03:05 -0500

    TODO Mock start of tests

--- testRunner.cpp
+++ testRunner.cpp
@@ -5,20 +5,22 @@ BufferedSerial pc(USBTX, USBRX);
 
 int main()
 {
-    printf("Hello World!\n");
     char buff[20] = "";
 
-    if(!pc.readable()) {
-        printf("Not readable\n");
-    }
-    while(1) {
+    do {
         if(pc.readable()) {
-            ThisThread::sleep_for(2000);
+            // TODO ThisThread::sleep_for(2000);
+            ThisThread::sleep_for(100);
             pc.read(buff, sizeof(buff));
-            if(strlen(buff) > 0) {
-                printf("Received: %s\n", buff);
-            }
             // TODO pc.write(buff, sizeof(buff));
+        //} else {
+        //    printf("Not readable\n");
         }
+    } while(strlen(buff) == 0);
+
+    printf("Received: %s\n", buff);
+    if(strncmp(buff, "Start now", strlen(buff)) == 0) {
+        printf("Start tests\n");
     }
+    while(1) {}
 }
