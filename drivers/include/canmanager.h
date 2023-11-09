f40105833b6a469fdf500f22d5d24532aed5ac63
Wilson Guo <wigu701@gmail.com>
2023-11-09 14:59:27 -0600

    CAN Driver Fixes
    Fixed error handler to occur periodically and properly reset bus with interrupt reattachment
    
    Fixed write queue buffer overflow by adding a timeout to the write function and retries on failure

--- drivers/include/canmanager.h
+++ drivers/include/canmanager.h
@@ -3,8 +3,10 @@
 
 #include "mbed.h"
 #include "platform/SharedPtr.h"
+#include <chrono>
 
-#define DEFAULT_CAN_FREQ 250000 // Match Elcon charger
+#define DEFAULT_CAN_FREQ 250000     // Match Elcon charger
+#define ERR_CHECK_INTERVAL 5000ms   // How quickly to check bus errors in ms
 
 
 /*
@@ -39,25 +41,28 @@
     3. Use sendMessage to write out to the bus at any time.
     - Example: 
     // Send CAN message with ID 0, data value 1, and length 1
-    SharedPtr<unsigned char> counter(new unsigned char[1]);
-    *counter = 1
-    bus.sendMessage(0x00, counter, 1);
+    char counter[] = {0};
+    bus.sendMessage(0x00, &counter, 1);
 
     4. Reads are automatically queued to be handled by the virtual readHandler function on interrupt
        Call the doRead function whenever the CPU is free to actually process the queued messages
     - Example
     // Process reads for up to 1ms time
-    bus.doRead(1ms);
+    bus.runQueue(1ms);
 */
 class CANManager {
     private:
         EventQueue messageQueue;    // Schedule reading messages
         CAN canBus;                 // Bus the class manages
+        Ticker errorClearer;        // Periodically clears error and ensures bus is working
 
         // Interrupt handler to queue a readMessage (cannot do actual read in IRQ context)
         void queueRead();
         // Takes CAN message out of buffer and queues handling of messages
         void readMessage();
+
+        // Interrupt handler to queue an errorHandler 
+        void queueError();
         // Prints out number of errors and resets bus on error interrupt
         void errorHandler();
 
@@ -66,7 +71,7 @@ class CANManager {
         // rd: CAN rd pin of the Nucleo to use
         // td: CAN td pin of the Nucleo to use
         // frequency: Baud rate of can bus
-        CANManager(PinName rd, PinName td, int frequency = DEFAULT_CAN_FREQ);
+        CANManager(PinName rd, PinName td, int frequency = DEFAULT_CAN_FREQ, std::chrono::milliseconds errorCheckPeriod = ERR_CHECK_INTERVAL);
 
         // Destructor stopping manager and freeing resources
         ~CANManager();
@@ -79,7 +84,7 @@ class CANManager {
         // messageID: CAN ID to use to identify the signal
         // data: Payload
         // length: Size of data in bytes
-        void sendMessage(int messageID, SharedPtr<unsigned char> data, int length);
+        int sendMessage(int messageID, void* data, int length, std::chrono::milliseconds timeout = 10ms);
 
         // Wrapper for Mbed CAN filter
         void filter(unsigned int id, unsigned int mask, CANFormat format=CANAny, int handle=0);
@@ -88,7 +93,7 @@ class CANManager {
         void frequency(int hz);
 
         // Processes queued reads to be handled for up to specified time
-        void doRead(std::chrono::milliseconds duration);
+        void runQueue(std::chrono::milliseconds duration);
 };
 
 
--- testRunner.cpp
+++ testRunner.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    while(1) {
+    while (1) {
         wait_us(1000000);
     }
 }
